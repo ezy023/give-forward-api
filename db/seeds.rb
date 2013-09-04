5a21aa5aa3282385cd310b4c8844356da4104b60
Justin McNally <justin@kohactive.com>
2013-09-04 17:21:19 -0500

    Update seeds.rb
    Move seeding to the seeds.rb, seeding is then done via rake db:seed rather than in the rails console.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Per the docs, why not just use the DB seed to do the seeding rather than the console
+
+4.times do |i|
+    Fundraiser.create(:name => "Fundraiser#{i}", :description => "Description #{i}")
+end
+
+# This file is executed via `rake db:seed`
